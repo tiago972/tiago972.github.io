a7ad568f10613f5394a45b07e90f93375d8726ff
Edouard Baudouin <Tiago@MacBook-Air-de-Edouard.local>
2019-01-02 19:32:55 +0100

    ok pour rendu

---
+++ main.js
@@ -0,0 +1,9 @@
+/*méthode setTimeout https://www.w3schools.com/jsref/met_win_settimeout.asp associée à confirm https://www.w3schools.com/jsref/met_win_confirm.asp dans une fonction*/
+setTimeout(function() {
+	let toPorn;
+	toPorn = confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please");
+	if (toPorn) 
+		window.location.href=("https://www.youtube.com/watch?v=dQw4w9WgXcQ");	 
+ }, 10000);
+
+
